c8fa53246d168094656923f9b439cc59689c2118
Klesh Wong <klesh@qq.com>
2019-08-21 23:02:09 +0800

    [feature] update doc

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['exists',['exists',['../classQCurl.html#ada4c77b4094cb3c8fda6814a1da200e5',1,'QCurl::exists(const QString &amp;path=QString())'],['../classQCurl.html#ab59468ee253effa5fa2af26b9b7a6550',1,'QCurl::exists(const QUrl &amp;url)'],['../classQCurlRequest.html#a46132dc2fb7e0415d7281a9b7c09c3fe',1,'QCurlRequest::exists()']]]
+  ['exists',['exists',['../classQCurl.html#ac5377ac480bf1344ce9601e7297a192f',1,'QCurl::exists(int &amp;result, const QString &amp;path=QString())'],['../classQCurl.html#a0f621f1cdfdefba8802587c6f902b7e9',1,'QCurl::exists(int &amp;result, const QUrl &amp;url)'],['../classQCurlRequest.html#ab6dc61067627c67e61b33f6905942ae1',1,'QCurlRequest::exists()']]]
 ];
